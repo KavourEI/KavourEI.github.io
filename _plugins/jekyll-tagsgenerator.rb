f42ce32b9aae3842e78c783ca55eef18c4e072da
Themis Kavour <69631669+KavourEI@users.noreply.github.com>
2024-06-12 16:49:12 +0300

    Update jekyll-tagsgenerator.rb

--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -1,43 +1,34 @@
 require 'slugify'
 module Jekyll
 
-  class TagsGenerator < Generator
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
 
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Posts tagged with \"#{tag}\""
+    end
+  end
+
+  class TagGenerator < Generator
     safe true
 
     def generate(site)
-      site.tags.each do |tag|
-        build_subpages(site, "tag", tag)
-      end
-    end
-
-    def build_subpages(site, type, posts)
-      posts[1] = posts[1].sort_by { |p| -p.date.to_f }
-      atomize(site, type, posts)
-      paginate(site, type, posts)
-    end
-
-    def atomize(site, type, posts)
-      path = "/tag/" + posts[0].slugify
-      atom = AtomPageTags.new(site, site.source, path, type, posts[0], posts[1])
-      site.pages << atom
-    end
-
-    def paginate(site, type, posts)
-      pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
-      (1..pages).each do |num_page|
-        pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
-        path = "/tag/" + posts[0].slugify
-        if num_page > 1
-          path = path + "/page#{num_page}"
+      if site.layouts.key? 'tag'
+        dir = 'tag'
+        site.tags.each_key do |tag|
+          site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
         end
-        newpage = GroupSubPageTags.new(site, site.source, path, type, posts[0])
-        newpage.pager = pager
-        site.pages << newpage
-
       end
     end
   end
+end
+
 
   class GroupSubPageTags < Page
     def initialize(site, base, dir, type, val)
